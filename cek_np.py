84776dd34383c27159c30d9655f56f67ad5a773b
mrsyntax25@gmail.com <mrsyntax25@gmail.com>
2025-05-06 09:04:46 +0700

    First Commit

---
+++ cek_np.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+# Check the version
+# print(np.__version__)
+
+# menghitung rata-rata, nilai maksimum, dan nilai minimum dari sebuah array
+
+# Data nilai siswa
+# nilai_siswa = np.array([85, 90, 78, 92, 88, 76, 95])
+
+# # Hitung statistik dasar
+# rata_rata = np.mean(nilai_siswa)
+# maksimum = np.max(nilai_siswa)
+# minimum = np.min(nilai_siswa)
+
+# # Tampilkan hasil
+# print("Data Nilai Siswa:", nilai_siswa)
+# print("Rata-rata Nilai:", rata_rata)
+# print("Nilai Tertinggi:", maksimum)
+# print("Nilai Terendah:", minimum)
+
+a2 = np.array([[1,2.0,3.3],
+               [4,5,6.5]])
+print (a2[1][2])
