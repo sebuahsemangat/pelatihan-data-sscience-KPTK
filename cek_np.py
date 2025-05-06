0f99cd0093a0cee47042ccfef7c5a8ae5ccea81b
mrsyntax25@gmail.com <mrsyntax25@gmail.com>
2025-05-06 09:13:15 +0700

    Array 3D Numpy

--- cek_np.py
+++ cek_np.py
@@ -19,6 +19,17 @@ import numpy as np
 # print("Nilai Tertinggi:", maksimum)
 # print("Nilai Terendah:", minimum)
 
-a2 = np.array([[1,2.0,3.3],
-               [4,5,6.5]])
-print (a2[1][2])
+# a2 = np.array([[1,2.0,3.3],
+#                [4,5,6.5]])
+# print (a2[1][2])
+
+# Array 3Dimensi
+a3 = np.array([[[ 1,  2,  3],
+        [ 4,  5,  6],
+        [ 7,  8,  9]],
+
+       [[10, 11, 12],
+        [13, 14, 15],
+        [16, 17, 18]]])
+
+print(a3[1][2][1])
